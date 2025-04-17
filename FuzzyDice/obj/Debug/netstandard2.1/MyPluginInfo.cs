a4c53d2ad963e6e2775977243be825fff9e07ee7
trou <144759552+trouDev@users.noreply.github.com>
2025-04-16 22:00:35 -0700

    FuzzyDice Template
    Used dotnet new lcharmony -n MyFirstMod -M MyName.MyFirstMod to create template for modding named FuzzyDice

---
+++ FuzzyDice/obj/Debug/netstandard2.1/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace FuzzyDice
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "Adam.FuzzyDice";
+        public const string PLUGIN_NAME = "FuzzyDice";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
